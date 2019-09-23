99e72cb6a51fe080de17c63282766ceb1c420e2d
LDHMX <2871679044@qq.com>
2019-09-24 00:24:02 +0800

    readeraction和service

---
+++ src/service/ReaderServicelmp.java
@@ -0,0 +1,12 @@
+package service;
+
+import model.Admin;
+
+/**
+* @author 
+* @version 创建时间：2019年9月24日 上午12:23:00
+* 
+*/
+public class ReaderServicelmp extends BaseService<Admin>implements ReaderService {
+
+}
