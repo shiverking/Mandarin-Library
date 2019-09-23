40ab3c74779735ced243aecc5c4227b1eb4af0b3
LDHMX <2871679044@qq.com>
2019-09-24 01:01:18 +0800

    修复reader spring配置

--- src/service/ReaderService.java
+++ src/service/ReaderService.java
@@ -4,6 +4,7 @@ package service;
 * @version 创建时间：2019年9月24日 上午12:19:42
 * 
 */
+import model.Reader;;
 public interface ReaderService {
 
 }
---
+++ src/service/ReaderServiceImpl.java
@@ -0,0 +1,12 @@
+package service;
+
+import model.Reader;
+
+/**
+* @author 
+* @version 创建时间：2019年9月24日 上午12:23:00
+* 
+*/
+public class ReaderServiceImpl extends BaseService<Reader>implements ReaderService {
+
+}
