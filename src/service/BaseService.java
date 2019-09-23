7d9bff7a04de52244572991bbc28d70249fe9a44
shiverking <35066603+shiverking@users.noreply.github.com>
2019-09-24 00:05:36 +0800

    Admin登陆功能实现
    实现了Admin的登陆功能

---
+++ src/service/BaseService.java
@@ -0,0 +1,17 @@
+package service;
+
+import dao.BaseDao;
+
+public class BaseService<TEntity>  {
+
+	private BaseDao<TEntity> dao;
+
+	protected BaseDao<TEntity> getDao() {
+		return dao;
+	}
+
+	public void setDao(BaseDao<TEntity> dao) {
+		this.dao = dao;
+	}
+	
+}
