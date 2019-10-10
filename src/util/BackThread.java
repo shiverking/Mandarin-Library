a7a37dd90e34912e0d14a9db93cdc30edf5f6875
LDHMX <2871679044@qq.com>
2019-10-10 21:01:12 +0800

    为book添加了Author和Introduction属性

--- src/util/BackThread.java
+++ src/util/BackThread.java
@@ -1,19 +1,22 @@
 package util;
+
+import java.util.Date;
+
 /**
 * @author 
 * @version 创建时间：2019年10月3日 下午7:31:29
 * 
 */
 public class BackThread extends Thread {
-	
+	Date todayDate;
 public void run() {
 	while (!this.isInterrupted()) {
+		todayDate=new Date();
 		try {
 			Thread.sleep(2000);
-			System.out.println("线程运行中");
-		}catch (Exception e) {
-			// TODO: handle exception
+		}catch (InterruptedException e) {
+			
 		}
-		
+		System.out.println(todayDate.toString()+this.getName());
 	}
 }}
