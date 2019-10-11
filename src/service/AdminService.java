aaf3e32346b0dd2c42c3186993336440b90bccc5
1026058431 <55679988+1026058431@users.noreply.github.com>
2019-10-11 15:29:40 +0800

    Revert "改"
    This reverts commit 5a1a38cc293918da41bdcf2392203f54e32745a3.

--- src/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import model.Admin;
-
-public interface AdminService {
-	public Admin verify(String AdminName, String Password);
-	public String changePassword(String AdminName,String NewPassword);
-}
