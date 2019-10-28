3cb4f118b97dd37c282fd14490919e699bb94a50
wjy19990621 <1209634642@qq.com>
2019-10-28 20:25:06 +0800

    日常修改。。。。

--- src/action/LibrarianAction.java
+++ src/action/LibrarianAction.java
@@ -50,6 +50,10 @@ public class LibrarianAction extends BaseAction<Librarian,LibrarianService> {
 		this.errorMessage="Your name or password is wrong, please try again !";
 		return INPUT;
 	}
+	public String signout() throws Exception {
+		ActionContext.getContext().getSession().clear();
+		return SUCCESS;
+	}
 	public String signup() throws Exception{
 		String LibrarianName = this.getModel().getLibrarianName();//閿熸枻鎷峰彇LibrarianName
 		String Password = this.getModel().getPassword();//閿熸枻鎷峰彇閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹閿燂拷
